bd11d02341e9d5887b2e2f805f0e4bf370ce8827
Fiezt <dat09t@gmail.com>
2023-04-17 14:11:58 +0700

    removed discord provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,9 @@
-import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord";
+import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID as string,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET as string
-        }),
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID as string,
             clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
