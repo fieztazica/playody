07327f4d7954d8b5ca9a3fe281914eafcffb04c5
Fiezt <dat09t@gmail.com>
2023-04-17 08:57:02 +0700

    edited 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 function NotFound() {
     return (
         <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
@@ -10,13 +12,13 @@ function NotFound() {
                     Sorry, we couldn’t find the page you’re looking for.
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <a
-                        href="#"
+                    <Link
+                        href="/"
                         className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Go back home
-                    </a>
-                    <a href="#" className="text-sm font-semibold text-gray-200">
+                    </Link>
+                    <a href="https://owlvernyte.tk/to" className="text-sm font-semibold text-gray-200">
                         Contact support <span aria-hidden="true">&rarr;</span>
                     </a>
                 </div>
