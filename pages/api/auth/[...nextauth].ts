18ebf8fab8b9e10befb1df8dd274c307405002e5
Fiezt <dat09t@gmail.com>
2023-04-17 15:44:58 +0700

    saved

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,44 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
+const spotifyScopes = encodeURIComponent([
+    "streaming",
+    "user-read-email",
+    "user-read-private",
+    "user-library-read",
+    "user-library-modify",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "playlist-read-private",
+    "playlist-read-collaborative",
+
+].join(" "))
+
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         SpotifyProvider({
             clientId: process.env.SPOTIFY_CLIENT_ID as string,
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+            authorization: `https://accounts.spotify.com/authorize?scope=${spotifyScopes}`
         })
     ],
+    callbacks: {
+        async session({ session, token, user }: any) {
+            session.user.id = token.id;
+            session.accessToken = token.accessToken;
+            return session;
+        },
+        async jwt({ token, user, account, profile, isNewUser }: any) {
+            if (user) {
+                token.id = user.id;
+            }
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+    },
 }
 
 export default NextAuth(authOptions)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,44 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { getToken } from 'next-auth/jwt'
+import { GetServerSidePropsContext } from 'next'
+import Script from 'next/script'
+import { useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const secret = process.env.NEXTAUTH_SECRET
 
 export default function Home() {
     const { data: session, status } = useSession()
+    console.log(session, status)
+
+    useEffect(() => {
+
+    }, [])
 
     return (
-        <main className="flex min-h-screen flex-col items-center p-24">
-            {status} as {session?.user?.name}
-            <p>{session?.user?.email}</p>
-            <a href="/api/auth/signin">Sign In</a>
-            <a href="/api/auth/signout">Sign Out</a>
-        </main>
+        <>
+            <Script
+                id="spotify-player"
+                src="https://sdk.scdn.co/spotify-player.js"
+            ></Script>
+            <main className="flex min-h-screen flex-col items-center p-24">
+                {status} as {session?.user?.name}
+                <p>{session?.user?.email}</p>
+                <Link href="/api/auth/signin">Sign In</Link>
+                <Link href="/api/auth/signout">Sign Out</Link>
+            </main>
+        </>
     )
+}
+
+export async function getServerSideProps({
+    req,
+    res,
+}: GetServerSidePropsContext) {
+    const token = await getToken({ req, secret })
+    console.log('JSON Web Token', token)
+    return {
+        props: {}, // will be passed to the page component as props
+    }
 }
