d85081ceb0991cf1f86505d0a47c0e56c615cce9
Fiezt <dat09t@gmail.com>
2023-04-19 17:31:19 +0700

    login middleware

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,39 @@
+import { ExtendedToken, TokenError } from "@/types";
+import { spotifyApi, scopes } from "@/lib/config/spotify";
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-const spotifyScopes = encodeURIComponent([
-    "streaming",
-    "user-read-email",
-    "user-read-private",
-    "user-library-read",
-    "user-library-modify",
-    "user-read-playback-state",
-    "user-modify-playback-state",
-    "playlist-read-private",
-    "playlist-read-collaborative",
+const spotifyScopes = encodeURIComponent(scopes)
 
-].join(" "))
+const refreshAccessToken = async (
+    token: ExtendedToken
+): Promise<ExtendedToken> => {
+    try {
+        spotifyApi.setAccessToken(token.accessToken)
+        spotifyApi.setRefreshToken(token.refreshToken)
 
-export const authOptions = {
+        // Refresh access token
+        const { body: refreshedTokens } = await spotifyApi.refreshAccessToken()
+
+        console.log('REFRESHED TOKENS ARE: ', refreshedTokens)
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            refreshToken: refreshedTokens.refresh_token || token.refreshToken,
+            accessTokenExpiresAt: Date.now() + refreshedTokens.expires_in * 1000
+        }
+    } catch (error) {
+        console.error(error)
+
+        return {
+            ...token,
+            error: TokenError.RefreshAccessTokenError
+        }
+    }
+}
+
+export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         SpotifyProvider({
@@ -23,22 +42,42 @@ export const authOptions = {
             authorization: `https://accounts.spotify.com/authorize?scope=${spotifyScopes}`
         })
     ],
+    pages: {
+        signIn: "/login"
+    },
+
     callbacks: {
         async session({ session, token, user }: any) {
-            session.user.id = token.id;
-            session.accessToken = token.accessToken;
+            session.accessToken = (token as ExtendedToken).accessToken
+            session.error = (token as ExtendedToken).error
             return session;
         },
         async jwt({ token, user, account, profile, isNewUser }: any) {
-            if (user) {
-                token.id = user.id;
+            let extendedToken: ExtendedToken
+
+            // User logs in for the first time
+            if (account && user) {
+                extendedToken = {
+                    ...token,
+                    user,
+                    accessToken: account.access_token as string,
+                    refreshToken: account.refresh_token as string,
+                    accessTokenExpiresAt: (account.expires_at as number) * 1000 // converted to ms
+                }
+
+                // console.log('FIRST TIME LOGIN, EXTENDED TOKEN: ', extendedToken)
+                return extendedToken
             }
-            if (account) {
-                token.accessToken = account.access_token;
+
+            // Subsequent requests to check auth sessions
+            if (Date.now() + 5000 < (token as ExtendedToken).accessTokenExpiresAt) {
+                // console.log('ACCESS TOKEN STILL VALID, RETURNING EXTENDED TOKEN: ', token)
+                return token
             }
-            return token;
+
+            // Access token has expired, refresh it
+            // console.log('ACCESS TOKEN EXPIRED, REFRESHING...')
+            return await refreshAccessToken(token as ExtendedToken)
         },
     },
-}
-
-export default NextAuth(authOptions)
+})
