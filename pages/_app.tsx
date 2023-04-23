614000f0d903f5294b39dd7c7d21e5a02935895a
Fiezt <dat09t@gmail.com>
2023-04-23 20:38:52 +0700

    chore: added chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { AppCtxProvider } from '@/lib/contexts/AppContext'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <SessionProvider session={pageProps.session}>
-            <AppCtxProvider>
-                <Component {...pageProps} />
-            </AppCtxProvider>
-        </SessionProvider>
+        <ChakraProvider>
+            <SessionProvider session={pageProps.session}>
+                <AppCtxProvider>
+                    <Component {...pageProps} />
+                </AppCtxProvider>
+            </SessionProvider>
+        </ChakraProvider>
     )
 }
