5180d0fa0f7e4c352e24adf50d0e5d33524547c6
Fiezt <dat09t@gmail.com>
2023-05-07 16:06:14 +0700

    dotenv added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 module.exports = {
     apps : [{
         name   : "playody",
-        script : "./server.js",
+        script : "server.js",
+        node_args : '-r dotenv/config',
         env_production: {
-            NODE_ENV: "production"
+            NODE_ENV: "production",
+            PORT: process.env.PORT
         },
         env_development: {
             NODE_ENV: "development"
