e7d43cf252ecdcb0b0ff533484dafa3615b60900
Fiezt <dat09t@gmail.com>
2023-05-07 16:23:36 +0700

    node args removed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps : [{
         name   : "playody",
         script : "server.js",
-        node_args : '-r dotenv/config',
         env_production: {
             NODE_ENV: "production",
         },
