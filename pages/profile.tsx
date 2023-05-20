1145cfb8bdcea879d0a4948d039716c30e524c90
phamtruong7302 <truongpham7302@gmail.com>
2023-05-20 21:50:04 +0700

    update

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -18,62 +18,6 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '@/typings/supabase'
 import Head from 'next/head'
 
-<<<<<<< HEAD
-const user = {
-  avatar_url:"#",
-  full_name: "Pham Huynh Nhat Truong",
-  username: "Tyui",
-  upload_at:"00:00",
-  email: "TyuiPahm@gmail.com",
-  location : "vietnam"
-}
-
-
- const Profile = () => {
-
-  const handleSubmit = () => {
-    // Perform your submit action here, e.g., update user data in the database
-    console.log("Submitting edited user:");
-  }
-  return (
-    <Box className='tw-h-full tw-bg-black/20' color="white" borderRadius="lg" p={16} maxWidth="100%" marginTop={0} overflow="hidden">
-  <Flex justifyContent="center">
-    <Box maxWidth="xl">
-      <Flex justifyContent="center">
-        <Box textAlign="center">
-          <Avatar src={user.avatar_url} size="xl" />
-          <Text mt={5} color="gray.500" fontSize="xl">
-            Full name: {user.full_name}
-          </Text>
-        </Box>
-      </Flex>
-      <VStack spacing={10} alignItems="flex-start" pt={10}>
-        <FormControl>
-          <FormLabel color="gray.500">Username</FormLabel>
-          <InputGroup size="md">
-            <Input defaultValue={user.username} />
-          </InputGroup>
-        </FormControl>
-        <FormControl>
-          <FormLabel color="gray.500">Update at</FormLabel>
-          <InputGroup size="md">
-            <Input defaultValue={user.upload_at} isReadOnly/>
-          </InputGroup>
-        </FormControl>
-      </VStack>
-      <Flex justifyContent="center" mt={10}>
-        <Button colorScheme="blue" onClick={handleSubmit}>
-          Submit
-        </Button>
-        <Button colorScheme="blue" marginLeft={1}>
-          My tracks
-        </Button>
-      </Flex>
-    </Box>
-  </Flex>
-</Box>
-  );
-=======
 type Props = {
     profile: Profile | null
 }
@@ -131,7 +75,6 @@ const MyProfile = ({profile}: Props) => {
            </Box>
        </>
     )
->>>>>>> main
 }
 
 MyProfile.getLayout = (page: React.ReactNode) => {
