98260ec360196e5a1055fc86da5597134f3a4d99
fiezt <dat09t@gmail.com>
2023-05-20 18:32:39 +0700

    disable zoom on mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,7 +44,7 @@ export default function App({ Component, pageProps: { initialSession, ...pagePro
         <>
             <Head>
                 <title>Playody</title>
-                <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
                 <meta name='title' content='Playody - Play your melody' />
                 <meta name='description' content='Playody is a free music sharing web-app' />
                 <meta name='keywords' content='music, sharing music, play, melody, playody' />
