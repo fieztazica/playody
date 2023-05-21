fa0b7ab27e8b4cfda28a15c49acbb473877b1641
fiezt <dat09t@gmail.com>
2023-05-21 12:52:47 +0700

    infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import { Box, Button, Input, Link, Text } from '@chakra-ui/react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Box, Button, Input, Link } from '@chakra-ui/react'
 import MainLayout from '@/components/MainLayout'
-import { PlayodyTitle } from '@/components/PlayodyTitle'
 import Head from 'next/head'
-import NextLink from 'next/link'
-import { TrackCard } from '@/components/TrackCard'
 import { Track, Profile } from '@/typings'
 import PostCard from '@/components/PostCard'
+import { useInViewport } from 'react-in-viewport'
 
 const Home = () => {
     const [page, setPage] = useState(1)
     const [isEnded, setIsEnded] = useState(false)
     const [posts, setPosts] = useState<(Track & { profiles: Profile | null })[]>([])
     const [loading, setLoading] = useState(false)
+    const loadBoxRef = useRef(null)
+    const {
+        inViewport,
+    } = useInViewport(
+        loadBoxRef,
+        undefined,
+        { disconnectOnLeave: false },
+        undefined,
+    )
 
     async function loadPosts() {
-       try {
-           setLoading(true)
-           const res = await fetch(`/api/track/list?page=${page}`).then(r => r.json())
-           console.log(res)
-           if (res.error) {
-               throw res.error
-           }
+        try {
+            setLoading(true)
+            const res = await fetch(`/api/track/list?page=${page}`).then(r => r.json())
+            console.log(res)
+            if (res.error) {
+                throw res.error
+            }
 
-           if (res.data.length) {
-               setPosts(a => [...a, ...res.data])
-           } else {
-               throw "List empty"
-           }
-       } catch (e: any) {
-           console.error(e)
-           setIsEnded(true)
-       } finally {
-           setLoading(false)
-       }
+            if (res.data.length) {
+                setPosts(a => [...a, ...res.data])
+            } else {
+                throw 'List empty'
+            }
+        } catch (e: any) {
+            console.error(e)
+            setIsEnded(true)
+        } finally {
+            setLoading(false)
+        }
     }
 
     useEffect(() => {
-        loadPosts()
+        if (!isEnded) loadPosts()
     }, [page])
+
+    useEffect(() => {
+        if (!isEnded && inViewport) setPage(p => p + 1)
+    }, [inViewport])
 
     return (
         <>
@@ -51,7 +62,7 @@ const Home = () => {
                         <PostCard track={v} />
                     </div>
                 ))}
-                <Button isLoading={loading} display={isEnded ? 'none' : 'flex'} onClick={() => {
+                <Button ref={loadBoxRef} isLoading={loading} display={isEnded ? 'none' : 'flex'} onClick={() => {
                     setPage(p => p + 1)
                 }}>
                     Load more
